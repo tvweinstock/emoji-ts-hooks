8de94a1b0c1b854a818d39b31458d4bbb3eb142d
Tobi Weinstock <tv.weinstock@gmail.com>
2019-12-03 12:39:23 +0100

    use query hook WIP

--- src/useAlgoliaQuery.ts
+++ src/useAlgoliaQuery.ts
@@ -2,33 +2,26 @@ import React, { useState, useEffect } from 'react';
 import useAlgoliaIndex from './useAlgoliaIndex';
 import { any } from 'prop-types';
 
-// Create a useQuery custom hook -- - It takes an indexName and a query string
+// useQuery custom hook, It takes an indexName and a query string
 const useAlgoliaQuery = (indexName: string, query: string) => {
   const AlgoliaIndex = useAlgoliaIndex(indexName);
-
   //   - It stores a list of hits (any[]) with useState. It’s initialised with an empty array
-  const [hits, setHits] = useState(<any>[]);
+  const [results, setResults] = useState(<any>[]);
 
-  // Every time the query or the algoliaIndex changes, it triggers a search. The hits from that search are stored in the state (use useEffect! https://reactjs.org/docs/hooks-reference.html#useeffect)
   useEffect(() => {
-    function handleQueryChange(query) {
-      setHits(query);
-    }
-
     AlgoliaIndex.search(
       {
-        query: evt.target.value,
+        query,
+        hitsPerPage: 50,
       },
       (err, { hits } = {}) => {
         if (err) throw err;
-
-        console.log(hits);
+        setResults(hits);
       }
     );
-    // It returns a list of hits
-  });
+  }, [indexName, query]); // Only re-run if query or indexname changes
 
-  return AlgoliaQuery;
+  return results;
 };
 
 export default useAlgoliaQuery;
